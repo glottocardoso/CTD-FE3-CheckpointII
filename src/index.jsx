8d235960e5ef04a3fd94775a611289ec7db9bcb9
glottocardoso <glottocardoso@gmail.com>
2023-06-26 22:47:00 -0300

    Commit inicial

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import Navbar from "./Components/Navbar";
+
 import Home from "./Routes/Home";
-import Footer from "./Components/Footer";
+import Login from "./Routes/Login";
+import Detail from "./Routes/Detail";
+
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //Lembre-se de configurar suas rotas e seu contexto aqui
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-    <Footer />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}>
+        <Route path="/" element={<Login />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/home" element={<Home />}/>
+        <Route path="/dentista/:id" element={<Detail />}/>
+      </Route>
+    </Routes>
+  </BrowserRouter> 
+   
 );
