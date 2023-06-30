952ef36af67a30edd632d95df194588800f5ea14
kenedfelix <kened.felix@gmail.com>
2023-06-29 21:12:50 -0300

    feito cadastro de consultas

--- src/Components/ScheduleForm.jsx
+++ src/Components/ScheduleForm.jsx
@@ -1,4 +1,6 @@
 import { useContext, useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import styles from "./ScheduleForm.module.css";
 import api from "../services/api";
 
@@ -27,6 +29,81 @@ const ScheduleForm = () => {
     }
   }
 
+  const toastConsultaOk = () => toast.success("Consulta agendada com sucesso", { autoClose: 4000, position: toast.POSITION.BOTTOM_CENTER });
+  const toastConsultaError = () => toast.error("Ocorreu um erro ao agendar a consulta.", { autoClose: 4000, position: toast.POSITION.BOTTOM_CENTER });
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const form = new FormData(event.target);
+      const dentistId = form.get("dentist");
+      const patientId = form.get("patient");
+      const appointmentDate = form.get("appointmentDate");
+
+      const selectedPaciente = pacientes.find((paciente) => paciente.matricula === patientId);
+      const selectedDentista = dentistas.find((dentista) => dentista.matricula === dentistId);
+
+      const payload = {
+        paciente: {
+          nome: selectedPaciente.nome,
+          sobrenome: selectedPaciente.sobrenome,
+          matricula: selectedPaciente.matricula,
+          usuario: {
+            username: selectedPaciente.usuario.username,
+          },
+          endereco: {
+            id: selectedPaciente.endereco.id,
+            logradouro: selectedPaciente.endereco.logradouro,
+            numero: selectedPaciente.endereco.numero,
+            complemento: selectedPaciente.endereco.complemento,
+            bairro: selectedPaciente.endereco.bairro,
+            municipio: selectedPaciente.endereco.municipio,
+            estado: selectedPaciente.endereco.estado,
+            cep: selectedPaciente.endereco.cep,
+            pais: selectedPaciente.endereco.pais,
+          },
+          dataDeCadastro: selectedPaciente.dataDeCadastro,
+        },
+        dentista: {
+          nome: selectedDentista.nome,
+          sobrenome: selectedDentista.sobrenome,
+          matricula: selectedDentista.matricula,
+          usuario: {
+            username: selectedDentista.usuario.username,
+          },
+        },
+        dataHoraAgendamento: appointmentDate,
+      };
+
+      const token = localStorage.getItem('token');
+
+      const response = await api.post("/consulta", payload, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+      
+        toastConsultaOk();
+        // Limpar os campos do formulário, se necessário
+      } else {
+        toastConsultaError();
+      }
+    } catch (error) {
+      console.log("Erro ao agendar consulta");
+      console.log(error);
+      toastConsultaError();
+    }
+
+    //Nesse handlesubmit você deverá usar o preventDefault,
+    //obter os dados do formulário e enviá-los no corpo da requisição 
+    //para a rota da api que marca a consulta
+    //lembre-se que essa rota precisa de um Bearer Token para funcionar.
+    //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
+  };
+
   useEffect(() => {
     const getDentistas = async () => {
       const dentistas = await getDentistasApi();
@@ -46,14 +123,6 @@ const ScheduleForm = () => {
     //Nesse useEffect, você vai fazer um fetch na api buscando TODOS os dentistas
     //e pacientes e carregar os dados em 2 estados diferentes
   }, []);
-
-  const handleSubmit = (event) => {
-    //Nesse handlesubmit você deverá usar o preventDefault,
-    //obter os dados do formulário e enviá-los no corpo da requisição 
-    //para a rota da api que marca a consulta
-    //lembre-se que essa rota precisa de um Bearer Token para funcionar.
-    //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
-  };
 
   return (
     <>
@@ -86,8 +155,8 @@ const ScheduleForm = () => {
               <select className="form-select" name="patient" id="patient">
                 {/*Aqui deve ser feito um map para listar todos os pacientes*/}
                 {pacientes.map(paciente => (
-                  <option key={paciente.matricula} value={paciente.matricula}>
-                    {paciente.nome +' '+ paciente.sobrenome}
+                  <option key={paciente.matricula} value={paciente.matricula} >
+                    {paciente.nome + ' ' + paciente.sobrenome}
                   </option>
                 ))}
 
@@ -119,6 +188,7 @@ const ScheduleForm = () => {
             </button>
           </div>
         </form>
+        <ToastContainer autoClose={4000} />
       </div>
     </>
   );
